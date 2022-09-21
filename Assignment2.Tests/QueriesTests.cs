4a1e2004880991ed5f6887e16296bfae54de2a81
Adrian Kari <adrka@itu.dk>
2022-09-21 15:53:14 +0200

    Implemented LINQ and Extension method queries Implemented LINQ and Extension method query tests

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -5,13 +5,82 @@ namespace Assignment2.Tests;
 public class QueriesTests
 {
     [Fact]
-    public void Test1()
+    public void Return_Names_Using_LINQ()
     {
         // Arrange
-        var names = Queries.ReturnNamesByRowling();
+        var names = Queries.ReturnNamesByRowlingLINQ();
 
         // Assert
         names.Should().BeEquivalentTo(new[] {"Albus Dumbledore", "Harry Potter", "Hermione Granger", "Minerva McGonagall", "Ronald Weasley", "Tom Riddle"});
+    }
 
+    [Fact]
+    public void Return_Names_Using_Extension_Methods()
+    {
+        // Arrange
+        var names = Queries.ReturnNamesByRowlingExtensionMethods();
+
+        // Assert
+        names.Should().BeEquivalentTo(new[] { "Albus Dumbledore", "Harry Potter", "Hermione Granger", "Minerva McGonagall", "Ronald Weasley", "Tom Riddle" });
+    }
+
+    [Fact]
+    public void Return_Year_First_Sith_LINQ()
+    {
+        // Arrange
+        var year = Queries.YearOfFirstSithLINQ();
+
+        // Assert
+        year.Should().Be(1977);
+    }
+
+    [Fact]
+    public void Return_Year_First_Sith_Extension_Methods()
+    {
+        // Arrange
+        var year = Queries.YearOfFirstSithExtensionMethods();
+
+        // Assert
+        year.Should().Be(1977);
+    }
+
+    [Fact]
+    public void Unique_List_Of_Wizards_LINQ()
+    {
+        // Arrange
+        var year = Queries.UniqueListOfWizardsLINQ();
+
+        // Assert
+        year.Should().BeEquivalentTo(new[] { ("Albus Dumbledore", 1997), ("Harry Potter", 1997), ("Ronald Weasley", 1997), ("Hermione Granger", 1997), ("Minerva McGonagall", 1997), ("Tom Riddle", 1998) });
+    }
+
+    [Fact]
+    public void Unique_List_Of_Wizards_Extension_Methods()
+    {
+        // Arrange
+        var year = Queries.UniqueListOfWizardsExtensionMethods();
+
+        // Assert
+        year.Should().BeEquivalentTo(new[] { ("Albus Dumbledore", 1997), ("Harry Potter", 1997), ("Ronald Weasley", 1997), ("Hermione Granger", 1997), ("Minerva McGonagall", 1997), ("Tom Riddle", 1998) });
+    }
+
+    [Fact]
+    public void Wizards_Grouped_By_Creator_Reverse_OrderBy_Creator_And_Wizard_Name_LINQ()
+    {
+        // Arrange
+        var wizards = Queries.WizardsGroupedByCreatorReverseOrderByCreatorAndWizardNameLINQ();
+
+        // Assert
+        wizards.Should().BeEquivalentTo(new[] { "Sauron", "Gandalf The White", "Gandalf The Grey", "Tom Riddle", "Ronald Weasley", "Minerva McGonagall", "Hermione Granger", "Harry Potter", "Albus Dumbledore", "Darth Vader" });
+    }
+
+    [Fact]
+    public void Wizards_Grouped_By_Creator_Reverse_OrderBy_Creator_And_Wizard_Name_Extensions_Methods()
+    {
+        // Arrange
+        var wizards = Queries.WizardsGroupedByCreatorReverseOrderByCreatorAndWizardNameExtensionMethods();
+
+        // Assert
+        wizards.Should().BeEquivalentTo(new[] { "Sauron", "Gandalf The White", "Gandalf The Grey", "Tom Riddle", "Ronald Weasley", "Minerva McGonagall", "Hermione Granger", "Harry Potter", "Albus Dumbledore", "Darth Vader" });
     }
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -4,7 +4,7 @@ namespace Assignment2;
 
 public class Queries
 {
-    public static String[] ReturnNamesByRowling()
+    public static String[] ReturnNamesByRowlingLINQ()
     {
         WizardCollection wizards = WizardCollection.Create();
 
@@ -25,6 +25,93 @@ public class Queries
 
 
         return returnNames.ToArray();
+    }
+
+    public static String[] ReturnNamesByRowlingExtensionMethods()
+    {
+        WizardCollection wizards = WizardCollection.Create();
+
+        var wizardNames = wizards.Where(a => a.Creator.Contains("Rowling")).Select(a => a.Name);
+
+        return wizardNames.ToArray();
+    }
+
+    public static int? YearOfFirstSithLINQ()
+    {
+        WizardCollection wizards = WizardCollection.Create();
+
+        var year = (from c in wizards
+                         where c.Name.Contains("Darth")
+                         orderby c.Year
+                         select new
+                         {
+                             c.Year
+                         }).First();
+
+        return year.Year;
+    }
+
+    public static int? YearOfFirstSithExtensionMethods()
+    {
+        WizardCollection wizards = WizardCollection.Create();
+
+        var year = wizards.Where(a => a.Name.Contains("Darth")).Select(a => a.Year).First();
+
+        return year;
+    }
+
+    public static List<(string, int?)> UniqueListOfWizardsLINQ()
+    {
+        WizardCollection wizards = WizardCollection.Create();
+
+        List<(string, int?)> wizardList = new List<(string, int?)>();
+
+        var wizardsNames = (from c in wizards
+                    where c.Medium.Contains("Harry Potter")
+                    select new
+                    {
+                        c.Name,
+                        c.Year
+                    }).Distinct();
+
+        foreach (var item in wizardsNames)
+        {
+            wizardList.Add((item.Name, item.Year));
+        }
+
+        return wizardList;
+    }
+
+    public static List<(string, int?)> UniqueListOfWizardsExtensionMethods()
+    {
+        WizardCollection wizards = WizardCollection.Create();
+
+        var wizardNames = wizards.Where(a => a.Medium.Contains("Harry Potter")).Select(a => (a.Name, a.Year)).Distinct();
+
+        return wizardNames.ToList();
+    }
+
+    public static List<string> WizardsGroupedByCreatorReverseOrderByCreatorAndWizardNameLINQ()
+    {
+        WizardCollection wizards = WizardCollection.Create();
+
+        var wizardNames = (from c in wizards
+                    orderby c.Name descending
+                    orderby c.Creator descending
+                    select c.Name);
+
+        return wizardNames.ToList();
+
+    }
+
+    public static List<string> WizardsGroupedByCreatorReverseOrderByCreatorAndWizardNameExtensionMethods()
+    {
+        WizardCollection wizards = WizardCollection.Create();
+
+        var wizardNames = wizards.OrderByDescending(a => a.Name).OrderByDescending(a => a.Creator).Select(a => a.Name);
+
+        return wizardNames.ToList();
+
     }
 
 }
